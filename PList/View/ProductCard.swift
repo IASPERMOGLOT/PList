a4cae15b5d553f5e3146f0b1a04c4e0e33ed11b6
IASPERMOGLOT <bulatooov@gmail.com>
2025-10-11 19:53:18 +0300

    SettingButton and ProductCard

--- PList/View/ProductCard.swift
+++ PList/View/ProductCard.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct ProductCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text("Название продукта")
+                            .padding()
+                            .font(.headline)
+                        
+                        Spacer()
+                        
+                        Button(action: {}) {
+                            Text("Готово")
+                                .padding()
+                                .foregroundColor(Color.black)
+                        }
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.brightGray)
+                            .frame(width: 370, height: 50)
+                            .shadow(radius: 5)
+                        
+                        HStack {
+                            Text("Описание, количество...")
+                        }
+                        .foregroundColor(Color.gray)
+                    }
+                    
+                    
+                    HStack {
+                        
+                        SettingButton(buttonName: "Изменить иконку", buttonImage: "pencil.and.scribble")
+                        
+                        SettingButton(buttonName: "Добавить фото", buttonImage: "photo")
+                    }
+                }
+            }
+            Button(action: {}) {
+                Text("Удальть товар")
+                    .padding()
+                    .foregroundColor(Color.white)
+            }
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.button)
+                )
+                .padding()
+        }
     }
 }
 
---
+++ PList/View/SettingButton.swift
@@ -0,0 +1,41 @@
+//
+//  SettingButton.swift
+//  PList
+//
+//  Created by Александр on 11.10.2025.
+//
+
+import SwiftUI
+
+struct SettingButton: View {
+    
+    var buttonName: String = "Null"
+    var buttonImage: String = "camera.metering.none"
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.brightGray)
+                .shadow(radius: 3)
+                .frame(width: 170 ,height: 100)
+            
+                .padding()
+            ZStack {
+                VStack {
+                    Image(systemName: buttonImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 30)
+                        .padding(10)
+                    
+                    Text(buttonName)
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingButton()
+}
